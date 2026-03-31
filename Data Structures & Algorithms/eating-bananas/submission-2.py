ca02f0a2fbea51bd1c88b38cd546bebcf1aa6dec
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-03-30 21:27:25 -0500

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        last_worked = -1 
+        while left <= right:
+            m = (left + right) // 2
+            tot = 0
+            for pile in piles:
+                tot += -(-pile // m)
+            if tot <= h:
+                last_worked = m
+                right = m - 1
+            else:
+                left = m + 1
+
+        return last_worked
