b6c2cfcbb4e40f2db9ece2bfc93eac942df3da9d
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-04-03 16:19:16 -0500

    Add: heap - submission-10

---
+++ Data Structures & Algorithms/heap/submission-10.py
@@ -0,0 +1,66 @@
+class MinHeap:
+    
+    def __init__(self):
+        self.heap = [0]
+
+    def push(self, val: int) -> None:
+        self.heap.append(val)
+        i = len(self.heap) - 1
+        while i > 1 and self.heap[i] < self.heap[i // 2]:
+            temp = self.heap[i]
+            self.heap[i] = self.heap[i // 2]
+            self.heap[i // 2] = temp
+            i //= 2
+    
+    def pop(self) -> int:
+        if len(self.heap) <= 1:
+            return -1
+        if len(self.heap) == 2:
+            return self.heap.pop()
+        
+        root = self.heap[1]
+        self.heap[1] = self.heap.pop()
+        i = 1
+        while 2 * i < len(self.heap):
+            if 2*i + 1 < len(self.heap) and \
+            self.heap[2*i + 1] < self.heap[2 * i] and \
+            self.heap[2*i + 1] < self.heap[i]:
+                temp = self.heap[i]
+                self.heap[i] = self.heap[2*i + 1]
+                self.heap[2*i + 1] = temp
+                i = 2*i + 1
+            elif self.heap[i] > self.heap[2 * i]:
+                temp = self.heap[i]
+                self.heap[i] = self.heap[2 * i]
+                self.heap[2 * i] = temp
+                i = 2 * i
+            else:
+                break
+        return root
+
+    def top(self) -> int:
+        if len(self.heap) <= 1:
+            return -1
+        return self.heap[1]
+
+    def heapify(self, nums: List[int]) -> None:
+        self.heap = [0] + nums
+        for curr in range(len(self.heap) // 2, 0, -1):
+            i = curr
+            while 2 * i < len(self.heap):
+                if 2*i + 1 < len(self.heap) and \
+                self.heap[2*i + 1] < self.heap[2 * i] and \
+                self.heap[2*i + 1] < self.heap[i]:
+                    temp = self.heap[i]
+                    self.heap[i] = self.heap[2*i + 1]
+                    self.heap[2*i + 1] = temp
+                    i = 2*i + 1
+                elif self.heap[i] > self.heap[2 * i]:
+                    temp = self.heap[i]
+                    self.heap[i] = self.heap[2 * i]
+                    self.heap[2 * i] = temp
+                    i = 2 * i
+                else:
+                    break
+        
+        
