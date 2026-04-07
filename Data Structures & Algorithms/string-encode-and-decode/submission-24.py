65d20a7b9f248a437a4b2a6cb01f62e003d08393
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-04-07 14:06:53 -0500

    Add: string-encode-and-decode - submission-24

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-24.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s))
+            res += ","
+        res += "#"
+        for s in strs:
+            res += s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        arr = []
+        lens = []
+        i = 0
+        curr = ""
+        while i < len(s):
+            if s[i] in "0123456789":
+                curr += s[i]
+            elif s[i] == ",":
+                lens.append(int(curr))
+                curr = ""
+            elif s[i] == "#":
+                i += 1
+                break
+            i += 1
+        for j in lens:
+            word = ""
+            for k in range(j):
+                word += s[i]
+                i += 1
+            arr.append(word)
+        return arr
