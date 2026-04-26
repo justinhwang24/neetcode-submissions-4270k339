e81cc5caf8f430343947879c4c701ae9c5361e26
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-04-26 12:22:45 -0500

    Add: valid-palindrome-ii - submission-16

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-16.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def isValidPalindrome(word):
+            l = 0
+            r = len(word) - 1
+            while l < r:
+                if word[l] != word[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        for i in range(len(s)):
+            if isValidPalindrome(s[:i] + s[i + 1:]):
+                return True
+        # return isValidPalindrome(s)
+        return False
