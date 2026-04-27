8461e168c0c1ecadc7f7a31ee6a469d24aee4ae8
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-04-27 13:01:12 -0500

    Add: validate-parentheses - submission-21

---
+++ Data Structures & Algorithms/validate-parentheses/submission-21.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parMap = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+        for c in s:
+            if c in '({[':
+                stack.append(parMap[c])
+            else:
+                if stack and stack[-1] == c:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
