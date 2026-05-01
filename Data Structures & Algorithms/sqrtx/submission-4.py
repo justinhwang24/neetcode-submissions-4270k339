579061b4ea0722f69bf875c2bc86cd333c44aea1
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-04-30 19:32:29 -0500

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+        while l <= r:
+            m = (l + r) // 2
+            if m ** 2 < x:
+                l = m + 1
+            elif m ** 2 > x:
+                r = m - 1
+            else:
+                return m
+        return r
