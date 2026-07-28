6ef7b7970c75906362a0d8039d2a3bcc9e2f5d3b
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-07-28 18:01:37 -0400

    Add: design-word-search-data-structure - submission-50

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-50.py
@@ -0,0 +1,34 @@
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(cur, i):
+            if i == len(word):
+                return cur.word
+            if word[i] == '.':
+                for child in cur.children.values():
+                    if dfs(child, i + 1):
+                        return True
+                return False
+            else:
+                if word[i] not in cur.children:
+                    return False
+                else:
+                    return dfs(cur.children[word[i]], i + 1)
+        
+        return dfs(self.root, 0)
