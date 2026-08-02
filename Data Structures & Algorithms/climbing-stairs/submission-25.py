46d2fb59a65cb505b7df8cd15f37a8effe160410
Justin Hwang <53981952+justinhwang24@users.noreply.github.com>
2026-08-02 14:57:43 -0400

    Add: climbing-stairs - submission-25

---
+++ Data Structures & Algorithms/climbing-stairs/submission-25.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev, cur = 1, 1
+        for i in range(n - 1):
+            temp = prev + cur
+            prev = cur
+            cur = temp
+        return cur
